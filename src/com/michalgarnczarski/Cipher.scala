47625d68009cca4c60cbba785612a2b76c3770a1
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-31 13:02:42 +0100

    reversedSymbols val added

--- src/com/michalgarnczarski/Cipher.scala
+++ src/com/michalgarnczarski/Cipher.scala
@@ -2,6 +2,8 @@ package com.michalgarnczarski
 
 abstract class Cipher(val symbols: List[Char]) {
 
+  val reversedSymbols: List[Char] = symbols.reverse
+
   // Function enabling both encrypting and decrypting of a given text. Decrypting works as encrypting but with
   // reversed alphabet.
 
