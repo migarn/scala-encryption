c714b064ce2eb2cdee38c3fab89e98d2d337457c
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-31 18:44:12 +0100

    CaesarCipher implemented completely

--- src/com/michalgarnczarski/CaesarCipher.scala
+++ src/com/michalgarnczarski/CaesarCipher.scala
@@ -1,41 +1,62 @@
 package com.michalgarnczarski
 
+import scala.annotation.tailrec
+
 class CaesarCipher(symbols: Array[Char], key: Int) extends Cipher(symbols) {
-  require(key <= symbols.size && key >= 0)
+  require(key <= symbols.length && key >= 0)
 
   override protected def transform(text: String, symbols: Array[Char]): String = {
 
-    println("text: " + text) // to be removed
+    // An auxiliary function implemented to transform text recursively.
 
+    @tailrec
     def auxiliaryTransform(toTransform: String, transformed: String): String = {
 
+      // If 'toTransform' is empty encoding/decoding terminates
+
       if (toTransform == "") transformed
+
+      // Otherwise first character of 'toTransform' is encoded/decoded and moved to 'transformed'. The function returns
+      // itself recursively with 'toTransform' without its first character and 'transformed' appended by this character
+      // after encoding/decoding.
+
       else {
+        val currentCharacter = toTransform.head
+        val newTransformed: String = defineNewTransformed(currentCharacter, transformed)
 
-        println("toTransform: " + toTransform + ", transformed: " + transformed) // to be removed
-
-        val character = toTransform.head
-
-        val newTransformed: String = {
-          if (symbols.contains(character)) {
-
-            val characterIndex = symbols.indexOf(toTransform.head)
-
-            val newcharacterIndex: Int = {
-              if (characterIndex + key > symbols.length - 1)
-                symbols.length - (characterIndex + key)
-              else characterIndex + key
-            }
-            println("characterIndex: " + characterIndex) // to be removed
-            transformed + symbols(newcharacterIndex)
-          }
-          else transformed + character
-        }
-        println("newTransformed: " + newTransformed) // to be removed
         auxiliaryTransform(toTransform.tail, newTransformed)
       }
     }
+
+    def defineNewTransformed(currentCharacter: Char, transformed: String): String = {
+
+      // Additional auxiliary function defined for clarity.
+      // If current character does not belong to symbols it is not transformed.
+
+      if (!symbols.contains(currentCharacter)) {
+        transformed + currentCharacter
+      }
+
+      else {
+        val characterIndex = symbols.indexOf(currentCharacter)
+
+        // Index of letter after transformation is determined regarding symbols array length. If index exceeds
+        // array's length it goes to the array's beginning.
+
+        val newCharacterIndex: Int = {
+
+          if (characterIndex + key > symbols.length - 1)
+            characterIndex + key - symbols.length
+
+          else characterIndex + key
+        }
+
+        transformed + symbols(newCharacterIndex)
+      }
+    }
+
+    // The function 'transform' returns tailrec function 'auxiliaryTransform'.
+
     auxiliaryTransform(text.toUpperCase, "")
   }
-
 }
--- src/com/michalgarnczarski/Main.scala
+++ src/com/michalgarnczarski/Main.scala
@@ -9,10 +9,10 @@ object Main {
 
     val symbols: Array[Char] = Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
     val alphabet: Alphabet = new Alphabet("Latin", symbols)
-    val caesarCipher: CaesarCipher = new CaesarCipher(symbols,1)
-    val str = "z"
+    val caesarCipher: CaesarCipher = new CaesarCipher(symbols,10)
+    val str = "Hello, world"
 
-//    println(caesarCipher.encryptOnlyLetters(str))
+    println(caesarCipher.encryptOnlyLetters(str))
     println(caesarCipher.encryptAll(str))
     println(caesarCipher.decrypt(caesarCipher.encryptAll(str)))
 
