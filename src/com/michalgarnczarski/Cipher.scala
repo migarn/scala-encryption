9ba9a44541b5a42638d63b478de8e492e1f14b78
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-31 13:01:24 +0100

    transform abstract function added

--- src/com/michalgarnczarski/Cipher.scala
+++ src/com/michalgarnczarski/Cipher.scala
@@ -1,5 +1,10 @@
 package com.michalgarnczarski
 
-abstract class Cipher(alphabet: Alphabet) {
+abstract class Cipher(val symbols: List[Char]) {
+
+  // Function enabling both encrypting and decrypting of a given text. Decrypting works as encrypting but with
+  // reversed alphabet.
+
+  protected abstract def transform(text: String, symbols: Array[Char])
 
 }
