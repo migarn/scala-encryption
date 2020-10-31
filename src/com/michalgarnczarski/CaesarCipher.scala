67bc3749d9836ee0ccca59eddfb9fff13d4e8d6c
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-31 15:47:29 +0100

    Require added to CaesarCIpher

--- src/com/michalgarnczarski/CaesarCipher.scala
+++ src/com/michalgarnczarski/CaesarCipher.scala
@@ -1,7 +1,10 @@
 package com.michalgarnczarski
 
-class CaesarCipher(symbols: List[Char], ) extends Cipher(symbols) {
+class CaesarCipher(symbols: List[Char], key: Int) extends Cipher(symbols) {
+  require(key <= symbols.size && key >= 0)
 
-  override def transform(text: String, symbols: List[Char]): String = ???
+  override def transform(text: String, symbols: List[Char]): String = {
+
+  }
 
 }
