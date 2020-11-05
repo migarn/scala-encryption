cd1ff2a3f1a913f73000a2569d3bae7369de9d10
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-05 19:48:34 +0100

    run implemented

--- src/com/michalgarnczarski/ConsoleApplication.scala
+++ src/com/michalgarnczarski/ConsoleApplication.scala
@@ -1,7 +1,6 @@
 package com.michalgarnczarski
 
 import com.michalgarnczarski.InputScanner.scanIntForSelectionList
-
 import scala.annotation.tailrec
 
 object ConsoleApplication {
@@ -29,7 +28,7 @@ object ConsoleApplication {
       "\n2 - to decrypt text" +
       "\n3 - to terminate",1,2,3)
 
-    println()
+    println("\nThe application enables encrypting and decrypting given text by means of two ciphers: Caesar cipher and Vigenere cipher.")
     auxiliaryRun(mainMenu())
   }
 }
--- src/com/michalgarnczarski/Main.scala
+++ src/com/michalgarnczarski/Main.scala
@@ -4,59 +4,18 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    // zrefaktorować, żeby tylko auxiliary transform było w poszczególnych klasach. Albo może nawer tylko defineNewTransformed
-
-    val array = Array(1,2,3)
-    println(array.length-1)
 
     val symbols: Array[Char] = Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 
-//    val s1 = "BABCIA"
-//    val s2 = "BABCIa"
-//    val s3 = "BAB CIA"
-//    val s4 = "BAB,CIA"
-//    val s5 = null
-//    val s6 = ""
-//
-//    println(s1.filter(c => !symbols.contains(c)) == "")
-//    println(s2.filter(c => !symbols.contains(c)) == "")
-//    println(s3.filter(c => !symbols.contains(c)) == "")
-//    println(s4.filter(c => !symbols.contains(c)) == "")
-//    println(s6.filter(c => !symbols.contains(c)) == "")
 
     val alphabet: Alphabet = new Alphabet("Latin", symbols)
     val caesarCipher: CaesarCipher = new CaesarCipher(symbols,10)
     val vigenereCipher: VigenereCipher = new VigenereCipher(symbols,"szyfr")
 
-    val str = "Hello, world"
-    val str2 = "Ala ma kota."
-
-//    println(caesarCipher.encryptOnlyLetters(str))
-//    println(caesarCipher.encryptAll(str))
-//    println(caesarCipher.decrypt(caesarCipher.encryptAll(str)))
-
-    println(vigenereCipher.encryptOnlyLetters(str2))
-    println(vigenereCipher.encryptAll(str2))
-    println(vigenereCipher.decrypt(vigenereCipher.encryptAll(str2)))
-
-    // Nie działa omijanie symboli spoza
+    println(ConsoleApplication.run(List(alphabet)))
 
 
 
-//    println(str.charAt(0))
-//
-//    println("Hello world.")
-
-    println("\n\n-------------------------------")
-
-
-    val vigenereCipher2: VigenereCipher = new VigenereCipher(symbols,"a")
-    val str3 = "mama"
-    println(vigenereCipher2.encryptOnlyLetters(str3))
-    println()
-    println(vigenereCipher2.encryptAll(str3))
-    println()
-    println(vigenereCipher2.decrypt(vigenereCipher2.encryptAll(str3)))
 
   }
 }
