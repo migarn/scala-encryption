1ffb5a546b13c55a1d2df3170bd2a9a1906f0a77
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-05 19:42:53 +0100

    run implemented

--- src/com/michalgarnczarski/ConsoleApplication.scala
+++ src/com/michalgarnczarski/ConsoleApplication.scala
@@ -1,5 +1,35 @@
 package com.michalgarnczarski
 
+import com.michalgarnczarski.InputScanner.scanIntForSelectionList
+
+import scala.annotation.tailrec
+
 object ConsoleApplication {
 
+  def run(alphabets: List[Alphabet]): String = {
+
+    @tailrec
+    def auxiliaryRun(controller: Int): String = {
+
+      if (controller == 3) "\nThe application has been terminated..."
+
+      else {
+        controller match {
+          case 1 => println("encrypt") // encryptdecrypt e
+          case 2 => println("decrypt") // encryptdecrypt d
+        }
+
+        val newController = mainMenu()
+        auxiliaryRun(newController)
+      }
+    }
+
+    def mainMenu(): Int = scanIntForSelectionList("\nType:" +
+      "\n1 - to encrypt text" +
+      "\n2 - to decrypt text" +
+      "\n3 - to terminate",1,2,3)
+
+    println()
+    auxiliaryRun(mainMenu())
+  }
 }
