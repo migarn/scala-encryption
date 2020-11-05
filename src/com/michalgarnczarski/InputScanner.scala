d64fd93dbc6ea4c100e9770f88d88ffdcf3b3c84
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-05 19:07:54 +0100

    scanIntForSelectionList added

--- src/com/michalgarnczarski/InputScanner.scala
+++ src/com/michalgarnczarski/InputScanner.scala
@@ -1,5 +1,31 @@
 package com.michalgarnczarski
 
+import scala.annotation.tailrec
+import scala.io.StdIn.readInt
+import scala.util.Try
+
 object InputScanner {
+
+  @tailrec
+  def scanIntForSelectionList(instruction: String, allowedInput: Int*): Int = {
+
+    // Function printing an instruction and returning an Int limited by parameters.
+    // The function works recursively until correct input is typed.
+
+    println(instruction)
+
+    // wrongInputValue is an Int returned for incorrect inputs. It is an Int which unfailingly is not an allowedInput.
+
+    val wrongInputValue: Int = allowedInput.max + 1
+    val input: Int = Try(readInt()).orElse(Try(wrongInputValue)).get
+
+    if (allowedInput.contains(input))
+      input
+
+    else {
+      println("\nWrong input!")
+      scanIntForSelectionList(instruction, allowedInput: _*)
+    }
+  }
 
 }
