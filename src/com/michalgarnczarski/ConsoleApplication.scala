1ec3437f887c891c8686d9deb3403a2b443e2aea
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-06 17:45:50 +0100

    Names changed in ConsoleApplication

--- src/com/michalgarnczarski/ConsoleApplication.scala
+++ src/com/michalgarnczarski/ConsoleApplication.scala
@@ -6,7 +6,7 @@ object ConsoleApplication {
 
   def run(alphabets: List[Alphabet]): String = {
 
-    def auxiliaryRun(controller: Int): String = {
+    def innerRun(controller: Int): String = {
 
       if (controller == 3) "\nThe application has been terminated..."
 
@@ -16,12 +16,12 @@ object ConsoleApplication {
           case 2 => encryptOrDecryptMenu(false)
         }
 
-        val newController = mainMenu()
-        auxiliaryRun(newController)
+        val newController = selectFromMenu()
+        innerRun(newController)
       }
     }
 
-    def mainMenu(): Int = scanIntForSelectionList("\nType:" +
+    def selectFromMenu(): Int = scanIntForSelectionList("\nType:" +
       "\n1 - to encrypt text" +
       "\n2 - to decrypt text" +
       "\n3 - to terminate",1,2,3)
@@ -29,10 +29,12 @@ object ConsoleApplication {
     println("\nThe application enables encrypting and decrypting given text by means of two ciphers:" +
       "Caesar cipher and Vigenere cipher.")
 
-    auxiliaryRun(mainMenu())
+    innerRun(selectFromMenu())
   }
 
   def encryptOrDecryptMenu(encrypt: Boolean): Boolean = {
+
+    def innerEncryptOrDecryptMenu(): Boolean = ???
 
     false
   }
