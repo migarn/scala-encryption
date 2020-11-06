67c4f6c2c1e4d0ca05ba9e5130c05be37ebcb61e
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-06 17:37:57 +0100

    encryptOrDecryptMenu function added

--- src/com/michalgarnczarski/ConsoleApplication.scala
+++ src/com/michalgarnczarski/ConsoleApplication.scala
@@ -1,21 +1,19 @@
 package com.michalgarnczarski
 
 import com.michalgarnczarski.InputScanner.scanIntForSelectionList
-import scala.annotation.tailrec
 
 object ConsoleApplication {
 
   def run(alphabets: List[Alphabet]): String = {
 
-    @tailrec
     def auxiliaryRun(controller: Int): String = {
 
       if (controller == 3) "\nThe application has been terminated..."
 
       else {
         controller match {
-          case 1 => println("encrypt") // encryptdecrypt e
-          case 2 => println("decrypt") // encryptdecrypt d
+          case 1 => encryptOrDecryptMenu(true)
+          case 2 => encryptOrDecryptMenu(false)
         }
 
         val newController = mainMenu()
@@ -28,7 +26,14 @@ object ConsoleApplication {
       "\n2 - to decrypt text" +
       "\n3 - to terminate",1,2,3)
 
-    println("\nThe application enables encrypting and decrypting given text by means of two ciphers: Caesar cipher and Vigenere cipher.")
+    println("\nThe application enables encrypting and decrypting given text by means of two ciphers:" +
+      "Caesar cipher and Vigenere cipher.")
+
     auxiliaryRun(mainMenu())
+  }
+
+  def encryptOrDecryptMenu(encrypt: Boolean): Boolean = {
+
+    false
   }
 }
