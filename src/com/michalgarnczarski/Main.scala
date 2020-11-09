3a2f5930693732a2a6af707ea9efb22cacea1aad
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-09 18:30:17 +0100

    Main implemented

--- src/com/michalgarnczarski/Main.scala
+++ src/com/michalgarnczarski/Main.scala
@@ -4,29 +4,17 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    val symbols: Array[Char] = Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    val latinSymbols: Array[Char] = Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+      'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    val polishSymbols: Array[Char] = Array('A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+      'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'R', 'S', 'Ś', 'T', 'U', 'W', 'Y', 'Z', 'Ź', 'Ż')
+    val polishExtendedSymbols: Array[Char] = Array('A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J',
+      'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ź', 'Ż')
 
+    val latinAlphabet: Alphabet = new Alphabet("Latin alphabet", latinSymbols)
+    val polishAlphabet: Alphabet = new Alphabet("Polish alphabet", polishSymbols)
+    val polishExtendedAlphabet: Alphabet = new Alphabet("Polish extended alphabet", polishExtendedSymbols)
 
-    val alphabet: Alphabet = new Alphabet("Latin", symbols)
-    val caesarCipher: CaesarCipher = new CaesarCipher(symbols,10)
-    val vigenereCipher: VigenereCipher = new VigenereCipher(symbols,"szyfr")
-
-    val consoleApplication = new ConsoleApplication(List(alphabet))
-    println(consoleApplication.run())
-
-//    symbols.foreach(s => println(s.getNumericValue))
-
-    val alphabet2: Alphabet = new Alphabet("Latin2", symbols)
-
-    val alphabets = List(alphabet)
-
-//    val allowedInput: List[Int] = for (a <- alphabets ::: List(0)) yield (alphabets ::: List(0)).indexOf(a) + 1
-
-    val allowedInput: List[Int] = (1 to alphabets.length).toList
-    println(allowedInput)
-
-
-
-
+    println(new ConsoleApplication(List(latinAlphabet, polishAlphabet, polishExtendedAlphabet)).run())
   }
 }
