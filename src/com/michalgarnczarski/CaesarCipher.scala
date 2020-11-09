a535523440514e45c27e00b7b887077f12fbc0dd
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-09 18:45:13 +0100

    CaesarCipher cleaned up

--- src/com/michalgarnczarski/CaesarCipher.scala
+++ src/com/michalgarnczarski/CaesarCipher.scala
@@ -1,36 +1,26 @@
 package com.michalgarnczarski
 
-import scala.annotation.tailrec
-
 class CaesarCipher(symbols: Array[Char], key: Int) extends Cipher(symbols) {
   require(key <= symbols.length && key >= 0)
 
   override protected def transform(text: String, innerSymbols: Array[Char]): String = {
 
-    // An auxiliary function implemented to transform text recursively.
-
-    @tailrec
-    def auxiliaryTransform(toTransform: String, transformed: String): String = {
-
-      // If 'toTransform' is empty encoding/decoding terminates
+    def transformInner(toTransform: String, transformed: String): String = {
 
       if (toTransform == "") transformed
 
-      // Otherwise the first character of 'toTransform' is encoded/decoded and moved to 'transformed'. The function returns
-      // itself recursively with 'toTransform' without its first character and 'transformed' appended by this character
-      // after encoding/decoding.
+      // If 'toTransform' is not empty the first character of 'toTransform' is encoded/decoded and moved to 'transformed'.
 
       else {
         val currentCharacter = toTransform.head
         val newTransformed: String = defineNewTransformed(currentCharacter, transformed)
 
-        auxiliaryTransform(toTransform.tail, newTransformed)
+        transformInner(toTransform.tail, newTransformed)
       }
     }
 
     def defineNewTransformed(currentCharacter: Char, transformed: String): String = {
 
-      // Additional auxiliary function defined for clarity.
       // If current character does not belong to symbols it is not transformed.
 
       if (!innerSymbols.contains(currentCharacter)) {
@@ -40,8 +30,7 @@ class CaesarCipher(symbols: Array[Char], key: Int) extends Cipher(symbols) {
       else {
         val characterIndex = innerSymbols.indexOf(currentCharacter)
 
-        // Index of letter after transformation is determined regarding symbols array length. If index exceeds
-        // array's length it goes to the array's beginning.
+        // If index exceeds array's length it goes to the array's beginning.
 
         val newCharacterIndex: Int = {
 
@@ -55,8 +44,6 @@ class CaesarCipher(symbols: Array[Char], key: Int) extends Cipher(symbols) {
       }
     }
 
-    // The function 'transform' returns tailrec function 'auxiliaryTransform'.
-
-    auxiliaryTransform(text.toUpperCase, "")
+    transformInner(text.toUpperCase, "")
   }
 }
